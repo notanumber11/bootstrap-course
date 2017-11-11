de226b7c22b9dd054c1c5fe2b4c0c8df08f6e2b5
notanumber11 <denisromasanta@gmail.com>
2017-11-11 14:12:22 +0100

    HTML templates

---
+++ js/utilitys.js
@@ -0,0 +1,7 @@
+// jQuery
+$.getScript('https://www.w3schools.com/lib/w3.js', function()
+{
+    // script is now loaded and executed.
+    // put your dependent JS here.
+    w3.includeHTML();
+});
