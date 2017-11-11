0bd274bf65768c1586f32ab6a474b1cabeab3111
notanumber11 <denisromasanta@gmail.com>
2017-11-11 16:15:34 +0100

    Add templates for include common html in the webpage Improve learnings from week 1

--- js/utilitys.js
+++ js/utilitys.js
@@ -1,5 +1,5 @@
 // jQuery
-$.getScript('https://www.w3schools.com/lib/w3.js', function()
+$.getScript('js/include-html.js', function()
 {
     // script is now loaded and executed.
     // put your dependent JS here.
