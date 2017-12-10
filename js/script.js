e622f169e5a13921043f9ed7d715b9f08c18c6cd
notanumber11 <denisromasanta@gmail.com>
2017-12-10 19:15:14 +0100

    Building and deployment with NPM scripts

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+$(document).ready(function(){
+    $('#mycarousel').carousel({
+        interval: 2000
+    })
+    $("#carousel-button").click(function(){
+        if ($("#carousel-button").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carousel-button").children("span").removeClass('fa-pause');
+            $("#carousel-button").children("span").addClass('fa-play');
+            $("#carousel-button").removeClass('btn-danger');
+            $("#carousel-button").addClass('btn-success');
+        }
+        else if ($("#carousel-button").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carousel-button").children("span").removeClass('fa-play');
+            $("#carousel-button").children("span").addClass('fa-pause');
+            $("#carousel-button").removeClass('btn-success');
+            $("#carousel-button").addClass('btn-danger');
+        }
+    });
+});
